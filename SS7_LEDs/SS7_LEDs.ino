054d6745c29dd6977b2726292a3fb139c6021d99
alyona-ra <alenkadar@gmail.com>
2022-04-06 13:31:57 -0400

    SS7:LEDs + Buttons

---
+++ SS7_LEDs/SS7_LEDs.ino
@@ -0,0 +1,31 @@
+const int button = 2;
+int buttonState = 0;
+int LEDs[] = {13, 12, 11, 10, 9, 8, 7, 6}; 
+
+void setup() {
+  for (int i = 0; i < 8; i++) {
+      pinMode(LEDs[i], OUTPUT);
+  }
+}
+
+void loop() {
+  buttonState = digitalRead(button);
+  delay(10);
+  if(buttonState == HIGH){
+    for (int i = 0; i < 8; i++) {
+        digitalWrite(LEDs[i], HIGH);
+        delay(100);
+        digitalWrite(LEDs[i], LOW);
+    }
+    for (int i = 6; i > 0; i--) {
+        digitalWrite(LEDs[i], HIGH);
+        delay(100);
+        digitalWrite(LEDs[i], LOW);
+    }
+      
+  } else {
+    for (int i = 0; i < 8; i++) {
+        digitalWrite(LEDs[i], LOW);
+    }
+  }
+}
